f591dfa1b76dcb02e44ce9402d9ad9ffc2aea0b4
Bzzyku <patryk.dlugolecki@o2.pl>
2023-06-01 14:26:28 +0200

    Tasty_App

---
+++ Tasty_Api_Result.py
@@ -0,0 +1,232 @@
+import os
+import requests
+from dotenv import load_dotenv
+from typing import Optional
+load_dotenv()
+from enum import Enum
+
+class Type_Goal(Enum):
+	maintain = "maintain"
+	lose = "lose"
+	gain = "gain"
+
+class Type_Sex(Enum):
+	male = "male"
+	female = "female"
+
+class Type_Activity_Level(Enum):
+	sedentary = "sedentary"
+	lightly_active = "lightly_active"
+	moderately_active = "moderately_active"
+	very_active = "very_active"
+
+class Tasty_App:
+	def __init__(self):
+		self.headers = {
+			"X-RapidAPI-Key": os.getenv("X_RapidAPI_Key"),
+			"X-RapidAPI-Host": os.getenv('X_RapidAPI_Host')
+		}
+	def get_recipies_auto_complete(self, Food_name_or_igredient: str):
+		url = "https://tasty.p.rapidapi.com/recipes/auto-complete"
+		querystring = {"prefix": Food_name_or_igredient}
+		response = requests.get(url, headers=self.headers, params=querystring)
+		result = response.json()
+		print(result)
+		return result
+	def get_recipes_list(self, range_from: str, range_to: str, tags : Optional[str] = "0", Food_name_or_igredient: Optional[str] = "0"):
+		url = "https://tasty.p.rapidapi.com/recipes/list"
+		if tags == "0" and Food_name_or_igredient == "0":
+			querystring = {"from": range_from, "size": range_to}
+		elif tags == "0" and Food_name_or_igredient != "0":
+			querystring = {"from": range_from, "size": range_to, "q": Food_name_or_igredient}
+		elif tags != "0" and Food_name_or_igredient == "0":
+			querystring = {"from": range_from, "size": range_to, "tags": tags}
+		else:
+			querystring = {"from": range_from, "size": range_to, "tags": tags, "q": Food_name_or_igredient}
+		response = requests.get(url, headers=self.headers, params=querystring)
+		result = response.json()
+		print(result)
+		return result
+	def get_recipes_list_similarities(self, recipe_id: str):
+		url = "https://tasty.p.rapidapi.com/recipes/list-similarities"
+		querystring = {"recipe_id": recipe_id}
+		response = requests.get(url, headers=self.headers, params=querystring)
+		result = response.json()
+		print(result)
+		return result
+	def get_recipes_get_more_info(self, recipe_id: str):
+		url = "https://tasty.p.rapidapi.com/recipes/get-more-info"
+		querystring = {"recipe_id": recipe_id}
+		response = requests.get(url, headers=self.headers, params=querystring)
+		result = response.json()
+		print(result)
+		return result
+	def get_tips_list(self, recipe_id: str, range_from: str, range_to: str):
+		url = "https://tasty.p.rapidapi.com/tips/list"
+		if range_from == "0" and range_to == "0":
+			querystring = {"id": recipe_id}
+		elif range_from == "0" and range_to != "0":
+			querystring = {"id": recipe_id, "size": range_to}
+		elif range_from != "0" and range_to == "0":
+			querystring = {"id": recipe_id, "from": range_from}
+		else:
+			querystring = {"id": recipe_id, "from": range_from, "size": range_to}
+		response = requests.get(url, headers=self.headers, params=querystring)
+		result = response.json()
+		print(result)
+		return result
+	def get_tags_list(self):
+		url = "https://tasty.p.rapidapi.com/tags/list"
+		response = requests.get(url, headers=self.headers)
+		result = response.json()
+		print(result)
+		return result
+	def get_feeds_list(self, range_to: str, timezone: str, vegetarian: bool ,range_from: str):
+		url = "https://tasty.p.rapidapi.com/feeds/list"
+		querystring = {"size": range_to, "timezone": timezone, "vegetarian": vegetarian, "from": range_from}
+		response = requests.get(url, headers=self.headers, params=querystring)
+		result = response.json()
+		print(result)
+		return result
+	def recipies_detail(self, recipe_id: str):
+		url = "https://tasty.p.rapidapi.com/recipes/detail"
+		querystring = {"id": recipe_id}
+		response = requests.get(url, headers=self.headers, params=querystring)
+		result = response.json()
+		print(result)
+		return result
+class Bespoke_Diet_Generator:
+	def __init__(self):
+		self.headers = {
+			'content-type': 'application/json',
+			'X-RapidAPI-Key': '396c88a5admshccd77e097b048c9p1ee005jsn313488fd1765',
+			'X-RapidAPI-Host': 'bespoke-diet-generator.p.rapidapi.com'
+	}
+	def create_new_user(self, height: int, weight: int, date_of_birth: str,  sex: str ,activity_level: str):
+		url = 'https://bespoke-diet-generator.p.rapidapi.com/user'
+		payload = {
+			"height": height,
+			"weight": weight,
+			"dateOfBirth": date_of_birth,
+			"sex": sex,
+			"activityLevel": activity_level
+		}
+		response = requests.post(url, json=payload, headers=self.headers)
+		result = response.json()
+		print(result)
+		return result
+	def get_users_information(self, userId: str ):
+		url = f"https://bespoke-diet-generator.p.rapidapi.com/user/{userId}"
+		response = requests.get(url, headers=self.headers)
+		result = response.json()
+		print(result)
+		return result
+	def update_user(self, userid: str, weight: int, activity_level: str):
+		url = f"https://bespoke-diet-generator.p.rapidapi.com/user/{userid}"
+		payload = {
+			"weight": weight,
+			"activityLevel": activity_level
+		}
+		response = requests.get(url, headers=self.headers.update({"Content-Type":"application/json"}))
+		result = response.json()
+		print(result)
+		return result
+class gym_calculator:
+	def __init__(self):
+		self.headers = {
+		"content-type": "application/json",
+		"X-RapidAPI-Key": "396c88a5admshccd77e097b048c9p1ee005jsn313488fd1765",
+		"X-RapidAPI-Host": "gym-calculations.p.rapidapi.com"
+	}
+
+	def bmi(self, weight: float, height: float):
+		url = "https://gym-calculations.p.rapidapi.com/bmi"
+
+		payload = {
+			"weight": weight,
+			"height": height
+		}
+
+		response = requests.post(url, json=payload, headers=self.headers)
+		result = response.json()
+		return result['result']
+	def calculate_macronutrient_ratios(self, goal: str, weight: float, height: float, age: int, sex: str, activity_level: str):
+		url = "https://gym-calculations.p.rapidapi.com/calculate-macronutrient-ratios"
+		payload = {
+			"goal": goal,
+			"weight": weight,
+			"height": height,
+			"age": age,
+			"gender": sex,
+			"activity_level": activity_level
+		}
+		response = requests.post(url, json=payload, headers=self.headers)
+		result = response.json()
+		print(result)
+		return result
+	def bmr(self, weight: float, height: float, age: int, sex: str):
+		url = "https://gym-calculations.p.rapidapi.com/bmr"
+		payload = {
+			"weight": 70,
+			"height": 1.8,
+			"age": 30,
+			"gender": "male"
+		}
+		response = requests.post(url, json=payload, headers=self.headers)
+		result = response.json()
+		print(result)
+		return result
+	def orm(self, weight_lifted: float, reps: int):
+		url = "https://gym-calculations.p.rapidapi.com/1rm"
+
+		payload = {
+			"weight_lifted": weight_lifted,
+			"reps": reps
+		}
+		response = requests.post(url, json=payload, headers=self.headers)
+		result = response.json()
+		print(result)
+		return result
+class Exercise:
+	def __init__(self):
+		self.headers = {"X-RapidAPI-Key": "396c88a5admshccd77e097b048c9p1ee005jsn313488fd1765",
+		"X-RapidAPI-Host": "musclewiki.p.rapidapi.com"
+		}
+	def get_exercise_by_id(self, id: int):
+		url = f"https://musclewiki.p.rapidapi.com/exercises/{id}"
+		response = requests.get(url, headers=self.headers)
+		result = response.json()
+		print(result)
+		return result
+	def get_attribiutes(self):
+		url = "https://musclewiki.p.rapidapi.com/exercises/attributes"
+		response = requests.get(url, headers=self.headers)
+		result = response.json()
+		# print(result['Yoga'])
+		return result
+	def get_exercises(self):
+		url = "https://musclewiki.p.rapidapi.com/exercises"
+		response = requests.get(url, headers=self.headers)
+		result = response.json()
+		print(result[0]['videoURL'])
+		return result
+
+	def filter_exercises(self, result, category: str, difficulty: str, muscles: str):
+		result = list(map(lambda el: el['videoURL'][0], filter(lambda el: el['Category'] == category
+																	   and el['Difficulty'] == difficulty
+																	   and el['target']['Primary'] == [muscles],
+															result)))[0]
+		return result
+
+Exercise_instance = Exercise()
+# Exercise_instance.get_exercise_by_id()
+Exercise_instance.get_attribiutes()
+
+# Tasty_App_Object = Tasty_App()
+# Tasty_App_Object.get_recipies_auto_complete('chicken')
+# generator = Bespoke_Diet_Generator()
+# generator.get_users_information('A5soqCXOD0PwrwUgjNATC')
+gym_calculator_instance = gym_calculator()
+# gym_calculator_instance.bmr(80.2, 172, 22, 'male')
+# gym_calculator_instance.bmi(70,180)
+# Exercise_instance.get_exercises()
--- Tasty_App.py
+++ Tasty_App.py
@@ -1,95 +1,131 @@
-import os
-import requests
-from dotenv import load_dotenv
-from typing import Optional
-load_dotenv()
-headers = {
-	"X-RapidAPI-Key": os.getenv("X_RapidAPI_Key"),
-	"X-RapidAPI-Host": os.getenv('X_RapidAPI_Host')
-}
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox, QComboBox
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtCore import QThread, Signal, Qt
+from PyQt6.QtGui import QDesktopServices
+from PyQt6.QtCore import QUrl
+import Tasty_Api_Result
+from typing import Dict
+from Tasty_Api_Result import gym_calculator, Tasty_App, Exercise, Bespoke_Diet_Generator, Type_Sex, Type_Goal, Type_Activity_Level
 
-def get_recipies_auto_complete(Food_name_or_igredient: str):
-	url = "https://tasty.p.rapidapi.com/recipes/auto-complete"
-	querystring = {"prefix": Food_name_or_igredient}
-	response = requests.get(url, headers=headers, params=querystring)
-	result = response.json()
-	print(result)
-	return result
+numbers = list(range(1, 101))
+numbers = [str(number) for number in numbers]
 
-def get_recipes_list(range_from: str, range_to: str, tags : Optional[str] = "0", Food_name_or_igredient: Optional[str] = "0"):
-	url = "https://tasty.p.rapidapi.com/recipes/list"
-	if tags == "0" and Food_name_or_igredient == "0":
-		querystring = {"from": range_from, "size": range_to}
-	elif tags == "0" and Food_name_or_igredient != "0":
-		querystring = {"from": range_from, "size": range_to, "q": Food_name_or_igredient}
-	elif tags != "0" and Food_name_or_igredient == "0":
-		querystring = {"from": range_from, "size": range_to, "tags": tags}
-	else:
-		querystring = {"from": range_from, "size": range_to, "tags": tags, "q": Food_name_or_igredient}
-	response = requests.get(url, headers=headers, params=querystring)
-	result = response.json()
-	print(result)
-	return result
+class Calculate_Macronutrients_Thread(QThread):
+    macronutrients_calculated = Signal(dict)
 
-def get_recipes_list_similarities(recipe_id: str):
-	url = "https://tasty.p.rapidapi.com/recipes/list-similarities"
-	querystring = {"recipe_id": recipe_id}
-	response = requests.get(url, headers=headers, params=querystring)
-	result = response.json()
-	print(result)
-	return result
+    def __init__(self, goal: str, weight: float, height: float, age: int, sex: str, activity_level: str):
+        super().__init__()
+        self.goal = goal
+        self.weight = weight
+        self. height = height
+        self.age = age
+        self.sex = sex
+        self.activity_level = activity_level
 
+    def run(self):
+        gym_calculator_instance = Tasty_Api_Result.gym_calculator()
+        result = gym_calculator_instance.calculate_macronutrient_ratios(self.goal, self.weight, self.height, self.age, self.sex, self.activity_level)
+        print(result)
+        self.macronutrients_calculated.emit(result)
+class BmiThread(QThread):
+    bmi_calculated = Signal(float)
 
-def get_recipes_get_more_info(recipe_id: str):
-	url = "https://tasty.p.rapidapi.com/recipes/get-more-info"
-	querystring = {"recipe_id": recipe_id}
-	response = requests.get(url, headers=headers, params=querystring)
-	result = response.json()
-	print(result)
-	return result
+    def __init__(self, weight, height):
+        super().__init__()
+        self.weight = weight
+        self.height = height
 
+    def run(self):
+        calculator = Tasty_Api_Result.gym_calculator()
+        result = calculator.bmi(self.weight, self.height)
+        self.bmi_calculated.emit(result)
 
-def get_tips_list(recipe_id: str, range_from: str, range_to: str):
-	url = "https://tasty.p.rapidapi.com/tips/list"
-	if range_from == "0" and range_to == "0":
-		querystring = {"id": recipe_id}
-	elif range_from == "0" and range_to != "0":
-		querystring = {"id": recipe_id, "size": range_to}
-	elif range_from != "0" and range_to == "0":
-		querystring = {"id": recipe_id, "from": range_from}
-	else:
-		querystring = {"id": recipe_id, "from": range_from, "size": range_to}
-	response = requests.get(url, headers=headers, params=querystring)
-	result = response.json()
-	print(result)
-	return result
+class Exercise_Thread(QThread):
+    exercise_result = Signal(str)
 
-def get_tags_list():
-	url = "https://tasty.p.rapidapi.com/tags/list"
-	response = requests.get(url, headers=headers)
-	result = response.json()
-	print(result)
-	return result
+    def __init__(self, result, categories, difficulties, muscles):
+        super().__init__()
+        self.categories = categories
+        self.difficulties = difficulties
+        self.muscles = muscles
+        self.result = result
+    def run(self):
+        browser = Tasty_Api_Result.Exercise()
+        result = browser.filter_exercises(result=self.result, category=self.categories, difficulty=self.difficulties, muscles=self.muscles)
+        self.exercise_result.emit(result)
 
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.resize(1600, 800)
+        loader = QUiLoader()
+        self.window = loader.load("interfejs.ui",self)
+        self.show()
+        Exercise_instance = Exercise()
+        result = Exercise_instance.get_exercises()
+        self.result = result
+        self.thread = None
 
-def get_feeds_list(range_to: str, timezone: str, vegetarian: bool ,range_from: str):
-	url = "https://tasty.p.rapidapi.com/feeds/list"
-	querystring = {"size": range_to, "timezone": timezone, "vegetarian": vegetarian, "from": range_from}
-	response = requests.get(url, headers=headers, params=querystring)
-	result = response.json()
-	print(result)
-	return result
+        self.window.Combo_Box_Type_Exercises.addItems(Exercise_instance.get_attribiutes()["categories"])
+        self.window.Combo_Box_Type_Difficulties.addItems(Exercise_instance.get_attribiutes()["difficulties"])
+        self.window.Combo_Box_Type_Muscles.addItems(Exercise_instance.get_attribiutes()["muscles"])
+        self.window.Combo_Box_Type_Goal.addItems([Goal.value for Goal in Type_Goal])
+        self.window.Combo_Box_Type_Sex.addItems([Sex.value for Sex in Type_Sex])
+        self.window.Combo_Box_Type_Activity_Level.addItems([Activity_Level.value for Activity_Level in Type_Activity_Level])
+        self.window.Combo_Box_Type_Age.addItems(numbers)
+        # connect the button to the on_push_button_clicked method
+        self.window.Push_Button_Oblicz.clicked.connect(self.on_push_button_clicked)
+        self.window.Push_Button_Find_Exercise.clicked.connect(self.on_push_button_find_exercise)
+        self.window.Push_Button_Calculate_Macronutrients.clicked.connect(self.on_push_button_calculate_macronutrients)
 
+    def on_push_button_clicked(self):
+        try:
+            weight = float(self.window.Line_Edit_Waga.text())
+            height = float(self.window.Line_Edit_Wzrost.text())
+        except ValueError:
+            QMessageBox.warning(self, "Błąd", "Wprowadź poprawne wartości dla wagi i wzrostu")
+            return
 
-def recipies_detail(recipe_id: str):
-	url = "https://tasty.p.rapidapi.com/recipes/detail"
-	querystring = {"id": recipe_id}
-	response = requests.get(url, headers=headers, params=querystring)
-	result = response.json()
-	print(result)
-	return result
+        self.thread = BmiThread(weight=weight, height=height)
+        self.thread.bmi_calculated.connect(self.on_bmi_calculated)
+        self.thread.start()
 
+    def on_push_button_find_exercise(self):
+        categories = str(self.window.Combo_Box_Type_Exercises.currentText())
+        difficulties = str(self.window.Combo_Box_Type_Difficulties.currentText())
+        muscles = str(self.window.Combo_Box_Type_Muscles.currentText())
+        self.thread = Exercise_Thread(result=self.result, categories=categories, difficulties=difficulties, muscles=muscles)
+        self.thread.exercise_result.connect(self.on_exercise_result)
+        self.thread.start()
 
-get_recipies_auto_complete('chicken')
-get_recipes_list("0","3","under_30_minutes","chicken")
-get_recipes_list_similarities("8138")
+    def on_push_button_calculate_macronutrients(self):
+        goal = str(self.window.Combo_Box_Type_Goal.currentText())
+        weight = float(self.window.Line_Edit_Waga.text())
+        height = float(self.window.Line_Edit_Wzrost.text())
+        age = int(self.window.Combo_Box_Type_Age.currentText())
+        sex = str(self.window.Combo_Box_Type_Sex.currentText())
+        activity_level = str(self.window.Combo_Box_Type_Activity_Level.currentText())
+        self.thread = Calculate_Macronutrients_Thread(goal=goal, weight=weight, height=height, age=age, sex=sex, activity_level=activity_level)
+        self.thread.macronutrients_calculated.connect(self.on_macronutrients_calculated)
+        self.thread.start()
+    def on_bmi_calculated(self, result):
+        self.window.Line_Edit_Bmi.setText(str(result))
+
+    def on_exercise_result(self, result):
+        self.window.Line_Edit_Exercises.clear()
+        self.window.Line_Edit_Exercises.setText(f'<a href="{result}">{result}</a>')
+        self.window.Line_Edit_Exercises.setTextFormat(Qt.RichText)
+        self.window.Line_Edit_Exercises.setTextInteractionFlags(Qt.TextBrowserInteraction)
+        self.window.Line_Edit_Exercises.setOpenExternalLinks(True)
+    def on_macronutrients_calculated(self, result):
+
+        self.window.Line_Edit_Calories.setText(str(result['result']['calories']))
+        self.window.Line_Edit_Protein.setText(str(result['result']['protein']))
+        self.window.Line_Edit_Carbs.setText(str(result['result']['carbs']))
+        self.window.Line_Edit_Fat.setText(str(result['result']['fat']))
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    sys.exit(app.exec())
